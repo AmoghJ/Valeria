1d3ba8326215fb7a4f711de1fc65b1237ca39ee4
AmoghJ <amoghjadhav14@gmail.com>
2017-05-15 18:59:25 -0400

    Added old stuff

---
+++ src/Scene.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+
+public class Scene {
+	
+	public String scene_name;
+	ArrayList<Scene> children;
+	
+	public Scene(String name) {
+		scene_name = name;
+		children = new ArrayList<Scene>();
+	}
+	
+	public void OutputTravelOptions() {
+		JOptionPane optionPane = new JOptionPane(null, JOptionPane.DEFAULT_OPTION,1);
+	}
+	
+}
